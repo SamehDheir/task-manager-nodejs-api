c6544661abbc2e93fa288f4966f1f938b7f471db
SamehDheir <dheir2000@gmail.com>
2025-02-28 11:01:03 +0200

    update

--- src/utils/generateToken.js
+++ src/utils/generateToken.js
@@ -23,7 +23,7 @@ exports.verifyToken = (req, res) => {
     return res.status(401).json({ message: "No token provided" });
   }
 
-  jwt.verify(token, "your-secret-key", (err, decoded) => {
+  jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
     if (err) {
       return res.status(401).json({ message: "Invalid or expired token" });
     }
